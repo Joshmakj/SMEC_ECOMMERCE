6da1128be4a92c0ba29b59978c63ea81a23e3c71
HAMADNM <hamadmoideen111@gmail.com>
2026-02-26 14:39:15 +0530

    dumb

--- seller/urls.py
+++ seller/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
           path("usersellerbridge/",views.user_seller_bridge,name="user_seller"),
+          path("sellerregistration/",views.seller_registration,name="seller_registration"),
 
 ]
--- seller/views.py
+++ seller/views.py
@@ -4,3 +4,5 @@ from core.models import User
 # Create your views here.
 def user_seller_bridge(request):
     return render(request,"seller/user_seller_bridge.html")
+def seller_registration(request):
+    return render(request,"seller/seller_registration.html")
