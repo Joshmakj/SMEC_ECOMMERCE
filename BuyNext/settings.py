2733cba15785efb6f23e0b81156e4d5fc638e5d6
Joshmakj <joshmakj2001@gmail.com>
2026-03-26 15:35:47 +0530

    Initial commit

--- BuyNext/settings.py
+++ BuyNext/settings.py
@@ -111,13 +111,11 @@ TEMPLATES = [
 
 
 load_dotenv()
-DEBUG = os.getenv("DEBUG")
-
-SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = True
+SECRET_KEY = "django-insecure-temporary-key"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
